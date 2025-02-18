88f1a824c75700e5d1aa53ea6b52e835db45e9db
Daniel.xiao <dxweixiao@tencent.com>
2025-02-18 15:15:50 +0800

    update

--- cloudrunfunction/index.js
+++ cloudrunfunction/index.js
@@ -1,3 +1,11 @@
+const cloudbase = require("@cloudbase/node-sdk");
+
 exports.main = function (event, context) {
+  // 调用 TCB 云开发的其他能力请参考: https://docs.cloudbase.net/cbrf/how-to-invoke-tcb
+  const app = cloudbase.init({
+    env: cloudbase.SYMBOL_CURRENT_ENV,
+    context
+  });
+
   return `hello world`;
 };
--- function/index.js
+++ function/index.js
@@ -1,8 +1,10 @@
-'use strict';
+const cloudbase = require("@cloudbase/node-sdk");
+
 exports.main = async (event, context) => {
-  console.log('Hello World');
-  console.log(event);
-  console.log(event['non-exist']);
-  console.log(context);
+  // 调用 TCB 云开发的其他能力请参考: https://docs.cloudbase.net/cloud-function/sdk
+  const app = cloudbase.init({
+    env: cloudbase.SYMBOL_CURRENT_ENV,
+  });
+
   return event;
 };
