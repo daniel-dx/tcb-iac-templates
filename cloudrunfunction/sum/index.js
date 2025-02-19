2083013d6db9c0addf95147636846b2226c9789c
Daniel.xiao <dxweixiao@tencent.com>
2025-02-19 17:49:25 +0800

    update

--- cloudrunfunction/sum/index.js
+++ cloudrunfunction/sum/index.js
@@ -1,5 +1,6 @@
 /**
  * 类型完整定义请参考：https://docs.cloudbase.net/cbrf/how-to-writing-functions-code#%E5%AE%8C%E6%95%B4%E7%A4%BA%E4%BE%8B
+ * 需要 `pnpm install` 安装依赖后类型提示才会生效
  * 
  * @type {import('@cloudbase/functions-typings').TcbEventFunction<{a: number, b: number}>}
  */
